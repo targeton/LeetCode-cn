71d772e2d9402d69b565a91f75aec10c84ca7763
zhoupeng <target_on@126.com>
2019-07-14 21:03:02 +0800

    solve No.203 problem.

---
+++ 203.移除链表元素.go
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=golang
+ *
+ * [203] 移除链表元素
+ *
+ * https://leetcode-cn.com/problems/remove-linked-list-elements/description/
+ *
+ * algorithms
+ * Easy (41.70%)
+ * Likes:    259
+ * Dislikes: 0
+ * Total Accepted:    29.9K
+ * Total Submissions: 71.6K
+ * Testcase Example:  '[1,2,6,3,4,5,6]\n6'
+ *
+ * 删除链表中等于给定值 val 的所有节点。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->6->3->4->5->6, val = 6
+ * 输出: 1->2->3->4->5
+ *
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeElements(head *ListNode, val int) *ListNode {
+	for head != nil && head.Val == val {
+		head = head.Next
+	}
+	cur := head
+	for cur != nil {
+		if cur.Next != nil && cur.Next.Val == val {
+			for cur.Next != nil && cur.Next.Val == val {
+				cur.Next = cur.Next.Next
+			}
+		}
+		cur = cur.Next
+	}
+	return head
+}
