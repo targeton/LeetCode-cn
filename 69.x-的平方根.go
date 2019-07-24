406ef7c5a14d3a36f013391b87ca1bd91a52dc5d
zhoupeng <target_on@126.com>
2019-07-24 17:55:17 +0800

    solve No.69 problem.(binary search)

---
+++ 69.x-的平方根.go
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=golang
+ *
+ * [69] x 的平方根
+ *
+ * https://leetcode-cn.com/problems/sqrtx/description/
+ *
+ * algorithms
+ * Easy (36.33%)
+ * Likes:    197
+ * Dislikes: 0
+ * Total Accepted:    45.8K
+ * Total Submissions: 125.9K
+ * Testcase Example:  '4'
+ *
+ * 实现 int sqrt(int x) 函数。
+ *
+ * 计算并返回 x 的平方根，其中 x 是非负整数。
+ *
+ * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+ *
+ * 示例 1:
+ *
+ * 输入: 4
+ * 输出: 2
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: 8
+ * 输出: 2
+ * 说明: 8 的平方根是 2.82842...,
+ * 由于返回类型是整数，小数部分将被舍去。
+ *
+ *
+ */
+func mySqrt(x int) int {
+	p, q := 0, x
+	for p <= q {
+		m := (p + q) / 2
+		v := m * m
+		if v > x {
+			q = m - 1
+		} else if v < x {
+			p = m + 1
+		} else {
+			return m
+		}
+	}
+	return q
+}
