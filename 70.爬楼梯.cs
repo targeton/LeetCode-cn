4d22ba71a2e83ba1be5aa162b9234fe235532ef2
zhoupeng <target_on@126.com>
2019-08-13 20:20:01 +0800

    solve No.70 problem.(use dynamic programming)

---
+++ 70.爬楼梯.cs
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=csharp
+ *
+ * [70] 爬楼梯
+ *
+ * https://leetcode-cn.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (46.32%)
+ * Likes:    575
+ * Dislikes: 0
+ * Total Accepted:    67.6K
+ * Total Submissions: 145.5K
+ * Testcase Example:  '2'
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 
+ * 注意：给定 n 是一个正整数。
+ * 
+ * 示例 1：
+ * 
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 
+ * 示例 2：
+ * 
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ * 
+ * 
+ */
+public class Solution {
+    public int ClimbStairs(int n) {
+        if(n <= 2)
+            return n;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i = 2; i < n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n-1];
+    }
+}
+
