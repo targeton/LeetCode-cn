a496aeb7324b229a1e542cb04e5b8be817b33b73
zhoupeng <target_on@126.com>
2019-09-12 22:18:41 +0800

    solve No.32 problem.(use dynamic programming)

--- 32.最长有效括号.cs
+++ 32.最长有效括号.cs
@@ -32,30 +32,22 @@
  */
 public class Solution {
     public int LongestValidParentheses(string s) {
-        Stack<int> stack = new Stack<int>();
-        for (int i = 0; i < s.Length; i++)
+        int max = 0;
+        int[] dp = new int[s.Length];
+        for (int i = 1; i < s.Length; i++)
         {
-            if(s[i] == '('){
-                stack.Push(i);
-            }else{
-                if(stack.Count() > 0 && s[stack.Peek()] == '('){
-                    stack.Pop();
+            if(s[i] == ')'){
+                if(s[i-1] == '('){
+                    dp[i] = (i-2) >= 0 ? dp[i-2]+2 : 2;
+                    max = Math.Max(dp[i],max);
                 }else{
-                    stack.Push(i);
+                    if(i-dp[i-1]-1 >= 0 && s[i-dp[i-1]-1] == '('){
+                        dp[i] = dp[i-1] + 2 + ((i-dp[i-1]-2 >= 0) ? dp[i-dp[i-1]-2] : 0);
+                        max = Math.Max(dp[i],max);
+                    }
                 }
             }
         }
-        int b = s.Length;
-        int a = 0;
-        int max = 0;
-        while(stack.Count()>0){
-            a = stack.Pop();
-            if(b-a > max)
-                max = b-a-1;
-            b = a;
-        }
-        if(b> max)
-            max = b;
         return max;
     }
 }
