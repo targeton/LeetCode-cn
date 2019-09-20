7c882785db1a3ff9073e8da80c275862103e2563
zhoupeng <target_on@126.com>
2019-09-20 20:22:37 +0800

    use BFS to solve No.127 problem.(Attention: use HashSet and StringBuilder to increase query rate and improve string operating speed)

---
+++ 127.单词接龙.cs
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode.cn id=127 lang=csharp
+ *
+ * [127] 单词接龙
+ *
+ * https://leetcode-cn.com/problems/word-ladder/description/
+ *
+ * algorithms
+ * Medium (36.74%)
+ * Likes:    134
+ * Dislikes: 0
+ * Total Accepted:    10.9K
+ * Total Submissions: 29.7K
+ * Testcase Example:  '"hit"\n"cog"\n["hot","dot","dog","lot","log","cog"]'
+ *
+ * 给定两个单词（beginWord 和 endWord）和一个字典，找到从 beginWord 到 endWord
+ * 的最短转换序列的长度。转换需遵循如下规则：
+ * 
+ * 
+ * 每次转换只能改变一个字母。
+ * 转换过程中的中间单词必须是字典中的单词。
+ * 
+ * 
+ * 说明:
+ * 
+ * 
+ * 如果不存在这样的转换序列，返回 0。
+ * 所有单词具有相同的长度。
+ * 所有单词只由小写字母组成。
+ * 字典中不存在重复的单词。
+ * 你可以假设 beginWord 和 endWord 是非空的，且二者不相同。
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * beginWord = "hit",
+ * endWord = "cog",
+ * wordList = ["hot","dot","dog","lot","log","cog"]
+ * 
+ * 输出: 5
+ * 
+ * 解释: 一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+ * ⁠    返回它的长度 5。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入:
+ * beginWord = "hit"
+ * endWord = "cog"
+ * wordList = ["hot","dot","dog","lot","log"]
+ * 
+ * 输出: 0
+ * 
+ * 解释: endWord "cog" 不在字典中，所以无法进行转换。
+ * 
+ */
+ using System.Collections.Generic;
+public class Solution {
+    public int LadderLength(string beginWord, string endWord, IList<string> wordList) {
+        var dict = new HashSet<string>(wordList);
+        if(!dict.Contains(endWord))
+            return 0;
+        var reached = new HashSet<string>();
+        reached.Add(beginWord);
+        int ladder = 0;
+        while (reached.Count() > 0)
+        {
+            ladder++;
+            var tmp = new HashSet<string>();
+            foreach (var word in reached)
+            {
+                if(word == endWord)
+                    return ladder;                
+                var sb = new StringBuilder(word);
+                for (int j = 0; j < sb.Length; j++)
+                {
+                    char old = sb[j];
+                    for(char c = 'a'; c <= 'z'; c++){
+                        sb[j] = c;
+                        if(dict.Contains(sb.ToString())){
+                            tmp.Add(sb.ToString());
+                            dict.Remove(sb.ToString());
+                        }
+                    }
+                    sb[j]=old;                    
+                }
+            }
+            reached = tmp;            
+        }
+        return 0;
+    }
+}
+
