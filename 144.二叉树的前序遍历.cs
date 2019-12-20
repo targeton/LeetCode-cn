2db3bca18356a0b1b3952307ebad571afa356219
zhoupeng <target_on@126.com>
2019-12-20 22:21:09 +0800

    use recursive algorithm to solve No.144 problem.

---
+++ 144.二叉树的前序遍历.cs
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=csharp
+ *
+ * [144] 二叉树的前序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Medium (61.74%)
+ * Likes:    181
+ * Dislikes: 0
+ * Total Accepted:    56.2K
+ * Total Submissions: 89.4K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树，返回它的 前序 遍历。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,null,2,3]  
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3 
+ * 
+ * 输出: [1,2,3]
+ * 
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public IList<int> PreorderTraversal(TreeNode root) {
+        var result = new List<int>();
+        if(root == null)
+            return result;
+        result.Add(root.val);
+        result.AddRange(PreorderTraversal(root.left));
+        result.AddRange(PreorderTraversal(root.right));
+        return result;
+    }
+}
+// @lc code=end
+
