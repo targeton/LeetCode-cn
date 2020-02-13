d827de04d1b27f0cffa200adca619c0d3e464fc0
zhoupeng <target_on@126.com>
2020-02-13 20:32:07 +0800

    solution for No.2 problem

---
+++ 2.两数相加.cs
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=csharp
+ *
+ * [2] 两数相加
+ *
+ * https://leetcode-cn.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (35.71%)
+ * Likes:    3891
+ * Dislikes: 0
+ * Total Accepted:    322.2K
+ * Total Submissions: 880.4K
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * 
+ * 示例：
+ * 
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        int sum = 0;
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        while(l1 != null || l2 != null){
+            sum /= 10;
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            head.next = new ListNode(sum % 10);
+            head = head.next;
+        }
+        if(sum / 10 == 1)
+            head.next = new ListNode(1);
+        return dummy.next;
+    }
+}
+// @lc code=end
+
