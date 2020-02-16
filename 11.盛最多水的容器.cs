72a249c2c091977fe5586064b90c0e67b359fb5f
zhoupeng <target_on@126.com>
2020-02-16 21:31:30 +0800

    modify,use two pointers to solve No.11 problem(time complexity:O(N))

--- 11.盛最多水的容器.cs
+++ 11.盛最多水的容器.cs
@@ -34,14 +34,14 @@
 // @lc code=start
 public class Solution {
     public int MaxArea(int[] height) {
-        int max = 0;
-        for (int i = 0; i < height.Length - 1; i++)
-        {
-            for (int j = i+1; j < height.Length; j++)
-            {
-                max = Math.Max(max,(j - i) * Math.Min(height[i], height[j]));
-            }
-        }
+        int max = 0, l = 0, r = height.Length - 1;
+        while(l < r){
+            max = Math.Max(max,(r - l) * Math.Min(height[l], height[r]));
+            if(height[l] < height[r])
+                l++;
+            else
+                r--;
+        }  
         return max;
     }
 }
