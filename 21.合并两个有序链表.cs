478510b685269e645135df9cca6cde9892f0f29d
zhoupeng <target_on@126.com>
2020-02-20 22:45:51 +0800

    use iteration to solve No.21 problem

---
+++ 21.合并两个有序链表.cs
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=csharp
+ *
+ * [21] 合并两个有序链表
+ *
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (57.34%)
+ * Likes:    859
+ * Dislikes: 0
+ * Total Accepted:    184.8K
+ * Total Submissions: 309.2K
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * 
+ * 示例：
+ * 
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        var dummy = new ListNode(0);
+        var head = dummy;
+        while (l1 != null && l2 != null)
+        {
+            if(l1.val < l2.val){
+                head.next = l1;
+                l1 = l1.next;
+            }else{
+                head.next = l2;
+                l2 = l2.next;
+            }
+            head = head.next;
+        }
+        head.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+}
+// @lc code=end
+
