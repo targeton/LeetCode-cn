7a0407d515529b89d62f15d8d52f726a14b3d04a
zhoupeng <target_on@126.com>
2020-02-23 23:21:19 +0800

    solve No.9 problem

--- 9.回文数.cs
+++ 9.回文数.cs
@@ -47,7 +47,7 @@ public class Solution {
         if (x < 0 || (x % 10 == 0 && x != 0))
             return false;
         int y = 0;
-        while(x < y){
+        while(x > y){
             y = y * 10 + x % 10;
             x = x / 10;
         }
