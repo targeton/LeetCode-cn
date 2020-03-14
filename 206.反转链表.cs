4927d39e60da107937749f559bab11131abeeaa7
zhoupeng <target_on@126.com>
2020-03-14 20:26:45 +0800

    solve No.206 problem.(reverse linked list)

---
+++ 206.反转链表.cs
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=csharp
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (64.51%)
+ * Likes:    839
+ * Dislikes: 0
+ * Total Accepted:    188.5K
+ * Total Submissions: 277K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 反转一个单链表。
+ * 
+ * 示例:
+ * 
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode tmp = null;
+        ListNode result = null;
+        while(head != null){
+            tmp = head.next;
+            head.next = result;
+            result = head;
+            head = tmp;
+        }
+        return result;
+    }
+}
+// @lc code=end
+
