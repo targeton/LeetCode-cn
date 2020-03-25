865520014bdae283bb0221323ee0f5a6ba97ef52
zhoupeng <target_on@126.com>
2020-03-25 15:01:04 +0800

    solve No.533 problem by math analysis

--- 25.k-个一组翻转链表.cs
+++ 25.k-个一组翻转链表.cs
@@ -51,31 +51,25 @@
  */
 public class Solution {
     public ListNode ReverseKGroup(ListNode head, int k) {
-        return ReverseKGroup(head, k, 1);
+        var cur = head;
+        int count = 0;
+        while(cur != null && count < k) {
+            cur = cur.next;
+            count += 1;
+        }
+        if(count == k) {
+            cur = ReverseKGroup(cur, k);
+            while(count > 0) {
+                var tmp = head.next;
+                head.next = cur;
+                cur = head;
+                head = tmp;
+                count -= 1;
+            }
+            head = cur;
+        }
+        return head;
     }
-
-    private ListNode ReverseKGroup(ListNode head, int k, int index) {
-        if(head == null) {
-            return head;
-        }
-        if(index == k) {
-            head.next = ReverseKGroup(head.next, k, 1);
-            return head;
-        }
-        var find = ReverseKGroup(head.next, k, index + 1);
-        if(find != null){
-            var tmp = head.next;
-            head.next = tmp.next;
-            tmp.next = head;
-            return find;
-        } else {
-            if (index == 1)
-                return head;
-            else 
-                return null;
-        }
-    }
-
 }
 // @lc code=end
 
