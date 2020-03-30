1e94b987e18522470041a0b893bea2c486a4cf9a
zhoupeng <target_on@126.com>
2020-03-30 23:45:21 +0800

    solve No.6 problem

---
+++ 6.z-字形变换.py
@@ -0,0 +1,67 @@
+#
+# @lc app=leetcode.cn id=6 lang=python3
+#
+# [6] Z 字形变换
+#
+# https://leetcode-cn.com/problems/zigzag-conversion/description/
+#
+# algorithms
+# Medium (47.12%)
+# Likes:    619
+# Dislikes: 0
+# Total Accepted:    111.4K
+# Total Submissions: 235.7K
+# Testcase Example:  '"PAYPALISHIRING"\n3'
+#
+# 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+# 
+# 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+# 
+# L   C   I   R
+# E T O E S I I G
+# E   D   H   N
+# 
+# 
+# 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+# 
+# 请你实现这个将字符串进行指定行数变换的函数：
+# 
+# string convert(string s, int numRows);
+# 
+# 示例 1:
+# 
+# 输入: s = "LEETCODEISHIRING", numRows = 3
+# 输出: "LCIRETOESIIGEDHN"
+# 
+# 
+# 示例 2:
+# 
+# 输入: s = "LEETCODEISHIRING", numRows = 4
+# 输出: "LDREOEIIECIHNTSG"
+# 解释:
+# 
+# L     D     R
+# E   O E   I I
+# E C   I H   N
+# T     S     G
+# 
+#
+
+# @lc code=start
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        rows = ['']*min(len(s),numRows)
+        going_down = False
+        cur = 0
+        for c in s:
+            rows[cur] += c
+            if cur == 0 or cur == numRows-1:
+                going_down = not going_down
+            cur += 1 if going_down else -1
+        return ''.join(rows)
+
+
+# @lc code=end
+
