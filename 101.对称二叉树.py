6e23b5128ba8d74f75654203732e3e7d2cb6993d
zhoupeng <target_on@126.com>
2020-04-07 12:57:59 +0800

    use recursion to solve No.101 problem.

---
+++ 101.对称二叉树.py
@@ -0,0 +1,62 @@
+#
+# @lc app=leetcode.cn id=101 lang=python3
+#
+# [101] 对称二叉树
+#
+# https://leetcode-cn.com/problems/symmetric-tree/description/
+#
+# algorithms
+# Easy (48.55%)
+# Likes:    661
+# Dislikes: 0
+# Total Accepted:    108K
+# Total Submissions: 214.1K
+# Testcase Example:  '[1,2,2,3,4,4,3]'
+#
+# 给定一个二叉树，检查它是否是镜像对称的。
+# 
+# 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+# 
+# ⁠   1
+# ⁠  / \
+# ⁠ 2   2
+# ⁠/ \ / \
+# 3  4 4  3
+# 
+# 
+# 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+# 
+# ⁠   1
+# ⁠  / \
+# ⁠ 2   2
+# ⁠  \   \
+# ⁠  3    3
+# 
+# 
+# 说明:
+# 
+# 如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def isMirror(left: TreeNode, right: TreeNode):
+            if left == None and right == None:
+                return True
+            if left == None or right == None:
+                return False
+            return left.val == right.val and isMirror(left.left, right.right) and isMirror(left.right, right.left)
+        
+        if not root:
+            return True
+        return isMirror(root.left, root.right)
+# @lc code=end
