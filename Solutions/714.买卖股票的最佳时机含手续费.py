e40938150eb65aa08eb155af6d26406dcebc8a3e
zhoupeng <target_on@126.com>
2020-06-04 21:03:17 +0800

    simplify code

--- Solutions/714.买卖股票的最佳时机含手续费.py
+++ Solutions/714.买卖股票的最佳时机含手续费.py
@@ -45,12 +45,10 @@
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        N = len(prices)
-        dp = [[0,0] for _ in range(N+1)]
-        dp[0][0], dp[0][1] = 0, float('-inf')
-        for i in range(N):
-            dp[i+1][1] = max(dp[i][1], dp[i][0] - prices[i])
-            dp[i+1][0] = max(dp[i][0], dp[i][1] + prices[i] - fee)
-        return dp[N][0]
+        cash, hold = 0, -prices[0]
+        for i in range(1, len(prices)):
+            cash = max(cash, hold + prices[i] - fee)
+            hold = max(hold, cash - prices[i])
+        return cash
 # @lc code=end
 
