a11cda9b0daf66f9431f04e71916e69b8c6b989a
pzhou <pzhou@mail.ie.ac.cn>
2021-01-28 19:12:46 +0800

    [modify], use dynamic programming with space compression to solve No.115 problem.

--- Solutions/115.不同的子序列.py
+++ Solutions/115.不同的子序列.py
@@ -72,16 +72,15 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         N, M = len(s), len(t)
-        dp = [[0 for i in range(M+1)] for j in range(N+1)]
-        for i in range(N+1):
-            dp[i][0] = 1
+        cur, prev = [0]*(M+1), [0]*(M+1)
+        cur[0], prev[0] = 1, 1
         for i in range(1, N+1):
             for j in range(1, M+1):
                 if s[i-1] == t[j-1]:
-                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j]
+                    cur[j] = prev[j-1] + prev[j]
                 else:
-                    dp[i][j] = dp[i-1][j]
-            
-        return dp[-1][-1]
+                    cur[j] = prev[j]
+            prev = list(cur)
+        return cur[-1]
 # @lc code=end
 
