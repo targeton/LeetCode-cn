98ca20b1be8f2315f733f636ee2a91d48935e273
pzhou <pzhou@mail.ie.ac.cn>
2021-06-03 16:43:21 +0800

    use dynamic programming to solve No.1563 problem.（Time complexity is from O(N^3) to O(N^2), but it's hard to understand.）

--- Solutions/1563.石子游戏-v.py
+++ Solutions/1563.石子游戏-v.py
@@ -61,27 +61,30 @@
 # @lc code=start
 class Solution:
     def stoneGameV(self, stoneValue: List[int]) -> int:
-        N = len(stoneValue)
-        pre = [0]*(N+1)
-        for i in range(N):
-            pre[i+1] = pre[i]+stoneValue[i]
+        n = len(stoneValue)
+        f = [[0] * n for _ in range(n)]
+        maxl = [[0] * n for _ in range(n)]
+        maxr = [[0] * n for _ in range(n)]
+
+        for left in range(n - 1, -1, -1):
+            maxl[left][left] = maxr[left][left] = stoneValue[left]
+            total = stoneValue[left]
+            suml = 0
+            i = left - 1
+            for right in range(left + 1, n):
+                total += stoneValue[right]
+                while i + 1 < right and (suml + stoneValue[i + 1]) * 2 <= total:
+                    suml += stoneValue[i + 1]
+                    i += 1
+                if left <= i:
+                    f[left][right] = max(f[left][right], maxl[left][i])
+                if i + 1 < right:
+                    f[left][right] = max(f[left][right], maxr[i + 2][right])
+                if suml * 2 == total:
+                    f[left][right] = max(f[left][right], maxr[i + 1][right])
+                maxl[left][right] = max(maxl[left][right - 1], total + f[left][right])
+                maxr[left][right] = max(maxr[left + 1][right], total + f[left][right])
         
-        dp = [[0 for _ in range(N+1)] for _ in range(N+1)]
-        
-        for s in range(2,N+1):
-            for i in range(N-s+1):
-                j = i + s
-                best = 0
-                for k in range(i+1,j):
-                    part1, part2 = pre[k]-pre[i], pre[j]-pre[k]
-                    if part1 == part2:
-                        best = max(best, part1 + max(dp[i][k], dp[k][j]))
-                    elif part1 > part2:
-                        best = max(best, part2 + dp[k][j])
-                    else:
-                        best = max(best, part1 + dp[i][k])
-                dp[i][j] = best
-        
-        return dp[0][N]
+        return f[0][n - 1]
 # @lc code=end
 
